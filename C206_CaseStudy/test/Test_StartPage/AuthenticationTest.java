93cc1d2836d90efabe7781b9b118f31b6f9d6d7a
22026163 <22026163@DESKTOP-D5ELPS7>
2023-07-29 00:23:35 +0800

    Added normal user, admin user and vender user along with their arraylist in-Brayden

--- C206_CaseStudy/test/Test_StartPage/AuthenticationTest.java
+++ C206_CaseStudy/test/Test_StartPage/AuthenticationTest.java
@@ -2,33 +2,51 @@ package Test_StartPage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AuthenticationTest {
+import HomePage.AdminUser;
+import HomePage.NormalUser;
+import HomePage.VenderUser;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+public class AuthenticationTest {
+    private NormalUser user1;
+    private NormalUser user2;
+    private NormalUser user3;
+    private NormalUser user4;
+    private AdminUser admin1;
+    private AdminUser admin2;
+    private VenderUser vendor1;
+    private VenderUser vendor2;
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    private ArrayList<NormalUser> normalUserList;
+    private ArrayList<AdminUser> adminUserList;
+    private ArrayList<VenderUser> venderUserList;
+    @BeforeAll
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @AfterAll
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testEnterName() {
+
+        fail("Not yet implemented");
+    }
 
 }
